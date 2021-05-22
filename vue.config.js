c8b32d83a04c0c84d53f7d73c0aa1a8277461461
Bryan Yunis <bbyunis@gmail.com>
2021-05-22 12:50:23 -0600

    change app name to foam soda

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Foam Soda";
+      return args;
+    });
+  },
   css: {
     loaderOptions: {
       sass: {
