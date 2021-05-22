402c4e4d24c5bdfb751761f5e0dabd0f40b88ad8
Bryan Yunis <bbyunis@gmail.com>
2021-05-22 16:02:23 -0600

    uninstall unused deps

--- src/main.js
+++ src/main.js
@@ -1,26 +1,19 @@
 // import style
 import 'swiper/components/navigation/navigation.scss'
 import 'swiper/components/pagination/pagination.scss'
-import 'swiper/swiper.min.css'
+import 'swiper/swiper-bundle.css'
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import VideoBg from 'vue-videobg'
-import vueVimeoPlayer from 'vue-vimeo-player'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.use(VueAwesomeSwiper, { autoHeight: true })
 
-Vue.component('video-bg', VideoBg)
-Vue.use(vueVimeoPlayer)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  components: {
-    VideoBg,
-  },
   render: (h) => h(App),
 }).$mount('#app')
